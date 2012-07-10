2649e411e4379e1f3b9f8f4ef1b6cea971491b07
Mike Flynn <mfflynn@gmail.com>
2012-07-10 11:42:13 -0500

    Created resize filter and updated the other stub filter functions.

--- src/trapperkeeper/filters.clj
+++ src/trapperkeeper/filters.clj
@@ -2,26 +2,46 @@
   (:use [clojure.java.shell :only [sh]]))
 
 (defn resize [params outfile infile]
-  (if (= 0 (:exit (sh "convert" (str tmp-img-dir "/*") "-append" outfile)))
-    true
-    false))
+; convert -resize '250'x -quality '75' -format jpg '$infile' '$outfile'
+  (let [
+    defaults {:w 250, :q 75}
+    args (merge defaults params)]
+    (if (= 0 (:exit (sh "convert" "-resize" (str (:w args) "x") (str "-quality '" (:q args) "'") "-format jpg" infile outfile)))
+      true
+      false)))
 
 (defn banner [params outfile infile]
-  (if (= 0 (:exit (sh "convert" (str tmp-img-dir "/*") "-append" outfile)))
-    true
-    false))
+; convert '$infile' -resize '100'x -gravity Center -crop '100'x'25'+0+0 +repage -format jpg -quality 91 '$outfile'	
+  (let [
+    defaults {:w 250, :q 75}
+    args (merge defaults params)]
+    (if (= 0 (:exit (sh "convert" "-resize" (str (:w args) "x") (str "-quality '" (:q args) "'") "-format jpg" infile outfile)))
+      true
+      false)))
 
 (defn newsprint [params outfile infile]
-  (if (= 0 (:exit (sh "convert" (str tmp-img-dir "/*") "-append" outfile)))
-    true
-    false))
+; convert -colorspace Gray '$infile' -thumbnail x'500' -resize ''500'x<' -resize 50% -gravity center -crop '250'x'250'+0+0 +repage -format jpg -quality 91 -ordered-dither h4x4a '$outfile'	
+  (let [
+    defaults {:w 250, :q 75}
+    args (merge defaults params)]
+    (if (= 0 (:exit (sh "convert" "-resize" (str (:w args) "x") (str "-quality '" (:q args) "'") "-format jpg" infile outfile)))
+      true
+      false)))
 
 (defn polaroid [params outfile infile]
-  (if (= 0 (:exit (sh "convert" (str tmp-img-dir "/*") "-append" outfile)))
-    true
-    false))
+; convert '$infile' -thumbnail '250'x -quality 75 -bordercolor snow +polaroid -background black \( +clone -shadow 60x4+4+4 \) +swap -background white -flatten '$outfile'	
+  (let [
+    defaults {:w 250, :q 75}
+    args (merge defaults params)]
+    (if (= 0 (:exit (sh "convert" "-resize" (str (:w args) "x") (str "-quality '" (:q args) "'") "-format jpg" infile outfile)))
+      true
+      false)))
 
 (defn thumb [params outfile infile]
-  (if (= 0 (:exit (sh "convert" (str tmp-img-dir "/*") "-append" outfile)))
-    true
-    false))
+; convert '$infile' -thumbnail x'500' -resize ''500'x<' -resize 50% -gravity center -crop '250'x'250'+0+0 +repage -format jpg -quality 91 '$outfile'
+  (let [
+    defaults {:w 250, :q 75}
+    args (merge defaults params)]
+    (if (= 0 (:exit (sh "convert" "-resize" (str (:w args) "x") (str "-quality '" (:q args) "'") "-format jpg" infile outfile)))
+      true
+      false)))

1a2b846790a8102f5bd271453da434a71284f7de
Mike Flynn <mfflynn@gmail.com>
2012-07-15 00:51:27 -0500

    Updated public path to work better with file compiled project.

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description "A clojure image server."
   :url "https://github.com/mikeflynn/trapperkeeper"
   :dependencies [[org.clojure/clojure "1.3.0"]
-  				[compojure "1.0.1"]
-  				[log4j "1.2.16"]
-  				[org.clojure/tools.logging "0.2.3"]
-  				[cheshire "4.0.0"]]
+                [compojure "1.1.1"]
+                [log4j "1.2.16"]
+                [org.clojure/tools.logging "0.2.3"]
+                [cheshire "4.0.0"]]
   :plugins [[lein-ring "0.6.2"]]
-  :ring {:handler trapperkeeper.core/app})
+  :ring {:handler trapperkeeper.core/app}
+  :resources-path "resources")
